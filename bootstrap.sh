2459410b6403945b117bc75ab20e89479280e1ed
Taishun Nakatani <teenak77@gmail.com>
2016-08-15 06:25:17 +0000

    Deleted coded for formatting node

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ ! -d "/hadoop/hdfs/cache/dfs/name/current" ]; then
-    #initialize name node
-    sudo -u hdfs hdfs namenode -format
-fi
 
 # When using service command it says "Failed to get D-Bus connection: Operation not permitted"
 # There is 3 ways to work around this phenomenon.
@@ -12,7 +8,6 @@ fi
 # 3. Avoid using service command and just use the daemon script directly
 # And my choice is 3. because the feature provided in service command is not actually needed at this morment.
 
-#service hadoop-hdfs-namenode start
 /etc/rc3.d/S85hadoop-hdfs-datanode start
 
 if [ $? -gt 0 ]; then
